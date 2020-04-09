cdcc869812ccd218a49dbe093e576e387160dabb
Jacek Gzel <jacek.gzel@gmail.com>
2020-04-09 23:14:20 +0200

    Init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Revenue from "./components/Revenue";
+
+function App() {
+  return (
+    <div className="App">
+      <Revenue />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Revenue.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Revenue extends React.Component{
+  render() {
+    return (
+      <h3>Revenue</h3>
+    );
+  }
+}
+
+export default Revenue;
