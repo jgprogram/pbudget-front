d8a9cfde76aa8fdee573d825e1ec1953b795ce13
jgprogram <jacek.gzel@gmail.com>
2020-09-08 22:28:38 +0000

    Deploying to master from  @ ac8fe9853261fde477327af398bada75581c8331 🚀

---
+++ precache-manifest.bbbbbe468ec9347c3c4829e3250ea07e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd0915d3b410a3d0e9eb5706f9933d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb9c118d3ad5ce141021",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "df10a0df82464f1fe80a",
+    "url": "/static/js/2.a5b82aa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5b82aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb9c118d3ad5ce141021",
+    "url": "/static/js/main.05004da8.chunk.js"
+  },
+  {
+    "revision": "66f9aae31153618a5ce3",
+    "url": "/static/js/runtime-main.f7c0cbae.js"
+  }
+]);
